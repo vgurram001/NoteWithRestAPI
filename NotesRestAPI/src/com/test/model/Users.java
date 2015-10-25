d6687206a9914535b811b56854e34bbf3c35d449
vgurram001 <gvenkat.vsoftcorp@gmail.com>
2015-10-25 13:55:44 +0530

    committed Users model file

---
+++ NotesRestAPI/src/com/test/model/Users.java
@@ -0,0 +1,62 @@
+package com.test.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Users {
+	
+	private String userId;
+	
+	private String emailId;
+
+	private String password;
+
+	private Date createTime;
+
+	private Date lastUpdateTime;
+	
+	
+
+	
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getLastUpdateTime() {
+		return lastUpdateTime;
+	}
+
+	public void setLastUpdateTime(Date lastUpdateTime) {
+		this.lastUpdateTime = lastUpdateTime;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+}
